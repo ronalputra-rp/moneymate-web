a666e9ef57bd4c096c13ce14a66d4fd1754836fe
ronalputra-rp <ronalputra.rpt@gmail.com>
2026-03-22 21:31:12 +0700

    feat: add balance storage controller

--- .js/storage.mjs
+++ .js/storage.mjs
@@ -1,6 +1,7 @@
 import {replaceData ,dataTransactions, addTransactions, editTransactions, deleteTransactions, getTransactions } from "./data.mjs";
     
-const STORAGE_KEY = "transactions"
+const STORAGE_KEY = "transactions";
+const STORAGE_BALANCE = "balance";
 
 export function saveTransactions (data) {
     localStorage.setItem(STORAGE_KEY,JSON.stringify(data));
@@ -16,4 +17,20 @@ export function loadTransactions() {
 
 export function clearTransactions() {
     localStorage.removeItem(STORAGE_KEY);
+}
+
+export function saveBalance (nominal) {
+    localStorage.setItem(STORAGE_BALANCE,Number(nominal));
+}
+
+export function loadBalance () {
+    const data = localStorage.getItem(STORAGE_BALANCE);
+    // if (!data) {
+    //     return "Belum ada data yang dimasukkan";
+    // }
+    return JSON.parse(data);
+}
+
+export function clearBalance () {
+    localStorage.removeItem(STORAGE_BALANCE);
 }
