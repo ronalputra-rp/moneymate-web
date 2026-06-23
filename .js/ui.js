ad043118606d5562d0839a6472d8632752fe9212
ronalputra-rp <ronalputra.rpt@gmail.com>
2026-06-23 19:35:11 +0700

    fix/feat: fix bug at deleteTransactions and add view all button

--- .js/ui.js
+++ .js/ui.js
@@ -10,22 +10,21 @@ import {
 import { loadBalance, loadTransactions, saveBalance } from "./storage.mjs";
 import { balance, dataTransactions, editBalance } from "./data.mjs";
 
-const stateData = dataTransactions;
-const finalData = stateData.sort((a, b) => {
+let stateData = dataTransactions;
+let finalData = stateData.sort((a, b) => {
     return a.id - b.id;
 });
+console.log(finalData);
 const balanceState = loadBalanceController();
 const transactionsData = loadTransactions();
-console.log(transactionsData);
 
-console.log(dataTransactions);
 
 const balanceWrapper = document.getElementById("balance-wrapper");
 const balanceInputSection = document.getElementById("balance-section");
 const balanceInput = document.getElementById("balance-input");
 const balanceButton = document.getElementById("balance-button");
 const toBalance = document.getElementById("to-balance");
-const idInput = document.getElementById("id-input");
+// const idInput = document.getElementById("id-input");
 const transactionInput = document.getElementById("trans-input");
 const typeInput = document.getElementById("type-input");
 const categoryInput = document.getElementById("category-input");
@@ -41,11 +40,6 @@ const addButton = document.getElementById("add-button");
 const balanceDataThumb = document.getElementById("balance-data-thumb");
 const buttonExpanded = document.getElementById("button-expanded");
 let editingId = null;
-
-// listDataButton.addEventListener("click",(e) => {
-// e.preventDefault();
-// wrapperRenderTransaction.classList.remove("hidden");
-// })
 
 let currentBalance = null;
 
@@ -83,12 +77,11 @@ function renderBalance() {
         return;
     }
     const balanceOutputWrap = document.createElement("section");
-    balanceOutputWrap.className =
-        "balance-out relative rounded-2xl px-4 sm:max-w-sm lg:max-w-lg hover:bg-emerald-600 hover:text-white";
+    balanceOutputWrap.className = "balance-out relative rounded-2xl px-4 sm:max-w-sm lg:max-w-lg";
     balanceOutputWrap.id = "balance-out-section";
     balanceWrapper.appendChild(balanceOutputWrap);
     const balanceOutputData = document.createElement("p");
-    balanceOutputData.className = "pl-2 mt-6 text-sm text-white font-semibold font-1";
+    balanceOutputData.className = "pl-2 mt-6 text-sm text-gray-300 font-semibold font-1";
     balanceOutputData.id = "balance-out-data";
     balanceOutputData.textContent = `Wallet Balance`;
     balanceOutputWrap.appendChild(balanceOutputData);
@@ -98,14 +91,15 @@ function renderBalance() {
     </svg>
     `;
     const balanceOutputNominal = document.createElement("p");
-    balanceOutputNominal.className = "pl-2 text-4xl mt-1 mb-4 font-3";
+    balanceOutputNominal.className = "pl-2 text-3xl lg:text-4xl mt-1 mb-4 font-3";
     balanceOutputNominal.id = "balance-out-nominal";
     balanceOutputNominal.textContent = `Rp. ${currentBalance.toLocaleString(`id-ID`)}`;
     balanceOutputWrap.appendChild(balanceOutputNominal);
     const hideNominal = document.createElement("button");
     hideNominal.id = "hide-nominal";
     hideNominal.type = "button";
-    hideNominal.className = "ml-2 absolute top-8 -translate-y-0.5 right-1/4 md:right-1/2 lg:right-1/2 translate-x-2";
+    hideNominal.className =
+        "ml-2 bg-linear-to-bl from-green-600 to-emerald-900 border-t border-l border-white/40 border-b border-r border-black/60 shadow-[inset_1px_1px_2px_rgba(255,255,255,0.1),_inset_-1px_-1px_2px_rgba(0,0,0,0.4)] rounded-xl p-1 absolute top-6 -translate-y-0.5 right-1/12 md:right-1/2 lg:right-1/2 translate-x-2";
     hideNominal.innerHTML = `${hideButton}`;
     balanceOutputWrap.appendChild(hideNominal);
     let isToggle = false;
@@ -140,8 +134,8 @@ function renderBalance() {
     editBalanceButton.type = "button";
     editBalanceButton.id = "edit-balance";
     editBalanceButton.className =
-        "p-3 gap-2 flex items-center bg-green-600 rounded-2xl text-slate-100 text-sm font-3 font-medium uppercase cursor-pointer ring-1 ring-white";
-    editBalanceButton.innerHTML = `${editIcon} Edit`;
+        "p-3 gap-2 flex items-center justify-center w-sm bg-green-700 transition-colors duration-150 ease-in-out hover:bg-linear-to-r hover:from-green-700 hover:to-emerald-900 rounded-2xl text-slate-100 text-sm font-3 font-medium uppercase cursor-pointer ring-1 ring-white";
+    editBalanceButton.innerHTML = `Edit ${editIcon}`;
     const deleteIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
     </svg>
@@ -150,8 +144,8 @@ function renderBalance() {
     deleteBalanceButton.type = "button";
     deleteBalanceButton.id = "delete-balance";
     deleteBalanceButton.className =
-        "delete-balance p-3 gap-2 flex items-center ml-4 bg-red-500 font-3 font-medium text-sm uppercase rounded-2xl cursor-pointer ring-1 ring-white";
-    deleteBalanceButton.innerHTML = `${deleteIcon} Delete`;
+        "delete-balance p-3 gap-2 w-sm flex items-center transition-colors duration-150 ease-in-out justify-center ml-4 bg-red-500 hover:bg-linear-to-l hover:from-red-500 hover:to-red-800 font-3 font-medium text-sm uppercase rounded-2xl cursor-pointer ring-1 ring-white";
+    deleteBalanceButton.innerHTML = `Delete ${deleteIcon}`;
     buttonSection.appendChild(editBalanceButton);
     buttonSection.appendChild(deleteBalanceButton);
     const balanceEdit = document.getElementById("edit-balance");
@@ -283,6 +277,17 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
+function viewALlConfiguration() {
+    const viewAllButton = document.getElementById("view-all-button");
+    const arrowRight = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 4.5 15 15m0 0V8.25m0 11.25H8.25" />
+</svg>
+`;
+    viewAllButton.innerHTML = `View All ${arrowRight}`;
+}
+
+viewALlConfiguration();
+
 function addRenderResult() {
     wrapperRenderTransaction.innerHTML = "";
     let options = {
@@ -291,15 +296,20 @@ function addRenderResult() {
         month: "long",
         day: "numeric",
     };
-
-    for (let i = 0; i < finalData.length; i++) {
-        const idResult = finalData[i].id;
-        const nameResult = finalData[i].transactionName;
-        const typeResult = finalData[i].type;
-        const categoryResult = finalData[i].category;
-        const nominalResult = Number(finalData[i].nominal);
+    // let dataTrial = stateData;
+    let dataRender = stateData.sort((a, b) => {
+        return a.id - b.id;
+    });
+    // console.log('Data asli', dataTrial);
+    console.log('Ini data buat render', dataRender);
+    for (let i = 0; i < dataRender.length; i++) {
+        const idResult = Number(dataRender[i].id);
+        const nameResult = dataRender[i].transactionName;
+        const typeResult = dataRender[i].type;
+        const categoryResult = dataRender[i].category;
+        const nominalResult = Number(dataRender[i].nominal);
         const nominalDisplay = nominalResult.toLocaleString(`id-ID`);
-        const dateResult = finalData[i].date;
+        const dateResult = dataRender[i].date;
         const dateParse = new Date(dateResult);
         const dateDisplay = dateParse.toLocaleDateString(`id-ID`, options);
 
@@ -312,10 +322,10 @@ function addRenderResult() {
         createRenderSection.className = "p-6";
         createResultSection.appendChild(createRenderSection);
         const getResultSection = document.getElementById(`transaction-${idResult}`);
-        const idRenderSection = document.createElement("p");
-        idRenderSection.id = "id-result";
-        idRenderSection.className = "font-1 text-xl";
-        idRenderSection.textContent = `${idResult}. ${nameResult}`;
+        const numberTransaction = document.createElement("p");
+        numberTransaction.id = "number-transaction";
+        numberTransaction.className = "font-1 text-xl";
+        numberTransaction.textContent = `${[i + 1]}. ${nameResult}`;
         const nominalSection = document.createElement("p");
         nominalSection.className = "px-4 py-1 mt-2 font-2 text-xl font-bold";
         nominalSection.id = "nominal-result";
@@ -339,15 +349,15 @@ function addRenderResult() {
         addEditButton.dataset.id = `${idResult}`;
         addEditButton.type = "button";
         addEditButton.className =
-            "edit-button p-3 mt-4 flex items-center gap-2 bg-green-600 lg:hover:bg-emerald-400 font-3 font-medium uppercase text-sm rounded-2xl ring-1 ring-white";
+            "edit-button p-3 mt-4 flex items-center gap-2 bg-green-700 hover:bg-linear-to-r hover:from-green-700 hover:to-emerald-900 transition-colors duration-150 ease-in-out rounded-2xl font-3 font-medium uppercase text-sm rounded-2xl ring-1 ring-white";
         addEditButton.innerHTML = `${editIcon} Edit`;
         const addDeleteButton = document.createElement("button");
         addDeleteButton.dataset.id = `${idResult}`;
         addDeleteButton.type = "button";
-        addDeleteButton.className = `delete-button p-3 mt-4 flex items-center gap-2 bg-red-500 hover:bg-emerald-400 font-3 font-medium uppercase text-sm rounded-2xl ring-1 ring-white`;
+        addDeleteButton.className = `delete-button p-3 mt-4 flex items-center gap-2 transition-colors duration-150 ease-in-out justify-center ml-4 bg-red-500 hover:bg-linear-to-l hover:from-red-500 hover:to-red-800  font-3 font-medium uppercase text-sm rounded-2xl ring-1 ring-white`;
         addDeleteButton.innerHTML = `${deleteIcon} Delete`;
 
-        getResultSection.appendChild(idRenderSection);
+        getResultSection.appendChild(numberTransaction);
         getResultSection.appendChild(dateSection);
         getResultSection.appendChild(nominalSection);
         getResultSection.appendChild(transButtonSection);
@@ -356,48 +366,99 @@ function addRenderResult() {
     }
 }
 
+let i = 1;
+let buttonDisabled = false;
 function handleSave(e) {
     e.preventDefault();
     const outputOption = document.getElementById("output-option");
-    const id = idInput.value.trim();
+    const id = i++;
     const transactionName = transactionInput.value.trim();
     const typeTransactions = typeInput.value.trim();
     const category = outputOption.textContent.trim();
     const nominal = nominalInput.value.trim();
     const date = dateInput.value.trim();
     const data = {
-        id: id,
+        id: Number(id),
         transactionName: transactionName,
         type: typeTransactions,
         category: category,
         nominal: Number(nominal),
         date: date,
     };
+    buttonDisabled = true;
+    if (buttonDisabled === true) {
+        const loading = `<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="shape-rendering: auto; display: block; background: transparent;" width="200" height="200" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-dasharray="164.93361431346415 56.97787143782138" r="35" stroke-width="10" stroke="#ffffff" fill="none" cy="50" cx="50">
+            <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="2.941176470588235s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+            </circle><g></g></g></svg>`;
+        saveButton.innerHTML = `${loading} Loading`
+    }
+    if (stateData.includes(id) === true) {
+        alert('Sudah terdapat data dengan ID yang sama !!');
+        inputSection.classList.add("hidden");
+        inputForm.classList.add("hidden");
+        return;
+    }
     if (editingId === null) {
         addController(data);
     } else {
         editController(editingId, data);
     }
     // wrapperRenderTransaction.innerHTML = "";
+    setTimeout(() => {
+        buttonDisabled = false;
+        if (buttonDisabled === false) {
+            saveButton.innerHTML = "Save Transactions";
+        }
+        inputSection.classList.add("hidden");
+        inputForm.classList.add("hidden");
+        alert("Data berhasil disimpan");
+    }, 2000)
     addRenderResult();
-    inputSection.classList.add("hidden");
-    inputForm.classList.add("hidden");
     buttonExpanded.classList.add("invisible");
     document.body.classList.remove("overflow-y-hidden");
 }
 
+let deleteClicked = false;
 function deleteEditConfiguration() {
     wrapperRenderTransaction.addEventListener("click", (e) => {
         e.preventDefault();
         const deleteBtn = e.target.closest(".delete-button");
+        const id = parseInt(e.target.dataset.id);
+        const loading = `<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="shape-rendering: auto; display: block; background: transparent;" width="200" height="200" xmlns:xlink="http://www.w3.org/1999/xlink"><g><circle stroke-dasharray="164.93361431346415 56.97787143782138" r="35" stroke-width="10" stroke="#ffffff" fill="none" cy="50" cx="50">
+            <animateTransform keyTimes="0;1" values="0 50 50;360 50 50" dur="2.941176470588235s" repeatCount="indefinite" type="rotate" attributeName="transform"></animateTransform>
+        </circle><g></g></g></svg>`;
         if (deleteBtn) {
-            const id = e.target.dataset.id;
-            deleteController(id);
-            alert("Transaksi berhasil dihapus");
-            addRenderResult();
-            console.log("After delete the data will be : ", stateData);
+            const id = parseInt(e.target.dataset.id);
+            const delay = (ms) => new Promise(resolve => setTimeout(resolve,ms));
+            console.log('Clicked in outside');
+            const handleDelete = async() => {
+                deleteClicked = true;
+                deleteBtn.innerHTML = `${loading} Loading`;
+                try {
+                    await Promise.all([
+                        deleteController(id),
+                        delay(800)
+                    ])
+                    const index = stateData.findIndex(n => String(n.id) === String(id));
+                    if (index !== -1) {
+                        stateData.splice(index,1);
+                    }
+                    console.log("After delete : ", stateData)
+                    alert("Transaksi berhasil dihapus");
+                    addRenderResult();
+                }
+                catch (error) {
+                    console.error('Terjadi Error' +  error.message)
+                }
+                finally {
+                    deleteClicked = false;
+                }
+                
+            }
+            handleDelete();
         }
         if (e.target.classList.contains("edit-button")) {
+            // wrapperRenderTransaction.innerHTML = "";
             saveButton.textContent = "Edit Transactions";
             inputSection.classList.toggle("hidden");
             inputForm.classList.remove("hidden");
@@ -414,8 +475,6 @@ addRenderResult();
 saveButton.addEventListener("click", handleSave);
 
 function filterOutput() {
-    console.log(transactionsData);
-    // const filteringCategory = transactionsData.filter() coming soon
     const dataTrans = document.getElementsByClassName("type-result");
     console.log(dataTrans);
 }
@@ -814,9 +873,4 @@ expandButton.addEventListener("click", (e) => {
     buttonExpanded.classList.toggle("invisible");
 });
 
-// toSummarize.addEventListener("click",(e) => {
-// summaryOutput(e);
-// });
-
-// summaryOutput();
-// console.log(dataTransactions);
+console.log(finalData);
